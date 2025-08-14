458a66fca23416bec5bd41dadafa5e4a4cca4254
SindhujaVivek <saisindhuja96@gmail.com>
2025-08-14 16:27:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Docker!"
+def index():
+    return "Welcome to the vulnerable Flask app!"
+
+# Arbitrary command execution (!!! DANGEROUS)
+@app.route('/ping')
+def ping():
+    ip = request.args.get('ip')
+    return os.popen(f"ping -c 1 {ip}").read()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Running with debug=True is a security risk
+    app.run(host='0.0.0.0', port=5000, debug=True)
